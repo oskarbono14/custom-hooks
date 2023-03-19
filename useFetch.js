e064ccc723517f86c08a95fa455692beaf771311
Oscar OM <oskarbono14@gmail.com>
2023-03-19 15:30:24 -0500

    Primer commit

---
+++ useFetch.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+
+
+export const useFetch = (url) => {
+    
+    const [state, setState] = useState({
+        data: null,
+        isLoading: true,
+        hasError: null,
+    })
+
+
+
+    const getFecht = async() =>{
+
+        setState({
+            ...state,
+            isLoading:true,
+        })
+
+
+        const resp = await fetch(url);
+        const data = await resp.json();
+
+        setState({
+            data,
+            isLoading:false,
+            hasError: null,
+        })
+    }
+
+    useEffect(() => {
+        getFecht();
+    }, [url])
+    
+  
+
+
+    return{
+        data: state.data,
+        isLoading: state.isLoading,
+        hasError: state.hasError,
+    };
+}
